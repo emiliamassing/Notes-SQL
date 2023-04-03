9b5ce967af5947ead3067c5e7bee63556b0a5a52
emiliamassing <emiliamassings@gmail.com>
2023-04-03 15:46:24 +0200

    ⚡️Created connection to database, added cors

--- server/app.js
+++ server/app.js
@@ -5,12 +5,20 @@ const logger = require('morgan');
 const cors = require('cors');
 const connection = require('./routes/conn');
 
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+connection.connect(function(err) {
+    if(err) {
+        console.log('Error', err);
+    };
+
+    console.log('Connected to database');
+});
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
