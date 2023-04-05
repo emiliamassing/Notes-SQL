9625114b2dffeee1c3984577642b03ec261007fb
emiliamassing <emiliamassings@gmail.com>
2023-04-05 13:40:27 +0200

    📝Added function to print note-form with tinymce

--- client/script/printDocuments.js
+++ client/script/printDocuments.js
@@ -4,20 +4,28 @@ let startpage = document.querySelector('.startpage');
 export function printEditor() {
     startpage.innerHTML = '';
 
+    let form = document.createElement('form');
+    let titleInput = document.createElement('input');
+    let summaryInput = document.createElement('textarea');
     let textArea = document.createElement('textarea');
+    let submitBtn = document.createElement('button');
+
     textArea.className = 'textContent';
 
-    startpage.appendChild(textArea);
+    submitBtn.innerHTML = 'Save Document';
+
+    form.append(titleInput, summaryInput, textArea, submitBtn);
+    startpage.appendChild(form);
 
     tinymce.init({
         selector: '.textContent',
         plugins: 'code',
         toolbar: 'undo redo | forecolor backcolor | stylesselect bold italic | alignleft alignright | code',
 
-        setup: function(editor) {
+        /*setup: function(editor) {
             editor.on('change', function(){
                 editor.save();
             });
-        }
+        }*/
     });
 };
